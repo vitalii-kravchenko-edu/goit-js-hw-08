933fb7f25c92902a8e6b576701125d03d3a5eab5
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-05 19:45:21 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime (data) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+
+const savedData = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+const currentTime = savedData.seconds;
+
+player.setCurrentTime(currentTime);
