9afa2eec48c199cf5281540ea445b047b7ec5b80
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-05 22:27:24 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,7 +10,7 @@ function saveCurrentTime (data) {
   localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
 };
 
-const savedData = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-const currentTime = savedData.seconds;
+const savedData = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? {};
+const currentTime = savedData.seconds ?? 0;
 
 player.setCurrentTime(currentTime);
