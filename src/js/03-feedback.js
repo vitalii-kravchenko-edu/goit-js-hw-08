338afc6c0924047a83f334357d020282f2fd2ef7
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-05 22:23:54 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+const data = {};
+
+form.addEventListener("input", throttle(saveInput, 500));
+form.addEventListener("submit", onSubmit);
+
+onPageReload();
+
+function saveInput () {
+  data.email = email.value;
+  data.message = message.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  saveInput();
+  console.log(data);
+  localStorage.clear();
+  form.reset();
+}
+
+function onPageReload() {
+  const savedData = JSON.parse(localStorage.getItem("feedback-form-state")) ?? {};
+  email.value = savedData.email ?? "";
+  message.value = savedData.message ?? "";
+}
